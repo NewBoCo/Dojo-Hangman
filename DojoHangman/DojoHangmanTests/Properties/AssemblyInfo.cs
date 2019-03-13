b66ddfe082fa3107ee604d384e5dc66e8c5636c5
Nate Adams <psiberknetic@gmail.com>
2019-03-13 10:59:40 -0500

    Initial Check-in: broken game

---
+++ DojoHangman/DojoHangmanTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DojoHangmanTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DojoHangmanTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d847633-0708-4c2f-8d66-c823e49e1526")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DojoHangman/HangmanConsoleGame/Program.cs
@@ -0,0 +1,74 @@
+﻿using HangmanLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanConsoleGame
+{
+    class Program
+    {
+        private static bool _playing = true;
+        private static string _playerName = String.Empty;
+
+        static void Main(string[] args)
+        {
+            _playerName = GetPlayerName();
+
+            while (_playing)
+            {
+                var currentGame = StartNewGame();
+
+                while (currentGame.WordNotGuessedYet && currentGame.GuessesRemaining > 0)
+                {
+                    UpdateScreenWithCurrentGuesses(currentGame);
+
+                    MakeAGuess(currentGame);
+                }
+
+                UpdateScreenWithCurrentGuesses(currentGame);
+
+                CongratulatePlayer();
+
+                _playing = AskIfPlayerWantsToPlayAgain();
+            }
+        }
+
+        private static HangmanGame StartNewGame()
+        {
+            return new HangmanGame(HangmanUtilities.GetRandomWord());
+        }
+
+        private static void UpdateScreenWithCurrentGuesses(HangmanGame currentGame)
+        {
+            Console.Clear();
+            Console.WriteLine($"Unguessed Letters: {currentGame.FormattedUnguessedLetters}");
+            Console.WriteLine($"Guesses Remaining: {currentGame.GuessesRemaining}");
+            Console.WriteLine("");
+            Console.WriteLine($"Word: {currentGame.MaskedWord}");
+        }
+
+        private static void CongratulatePlayer()
+        {
+            Console.WriteLine($"CONGRATULATIONS, {_playerName}. You guessed it!");
+            Console.ReadKey();
+        }
+
+        private static bool AskIfPlayerWantsToPlayAgain()
+        {
+            return true;
+        }
+
+        private static void MakeAGuess(HangmanGame currentGame)
+        {
+            var guess = Console.ReadKey().KeyChar;
+            currentGame.MakeGuess(guess);
+        }
+
+        private static string GetPlayerName()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
